24f784b250800d7a3e7fc18df3fb06f28d19d90d
kono <konotorie@gmail.com>
2019-12-13 21:18:32 +0900

    create nn and ready for training

--- classify_cifar.py
+++ classify_cifar.py
@@ -5,7 +5,9 @@ https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#sphx-glr-begi
 
 import torch
 import torchvision
+import torch.optim as optim
 import torchvision.transforms as transforms
+from architecture import Net
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -23,3 +25,26 @@ test_loader = torch.utils.data.DataLoader(test_set, batch_size=4,
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')                            
+
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SSD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finish training')            
